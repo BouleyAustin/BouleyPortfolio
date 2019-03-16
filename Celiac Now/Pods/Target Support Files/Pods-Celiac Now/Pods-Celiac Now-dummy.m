306bc84136104b8b12609b3bf102cbef0365d258
Austin Bouley <bouleyaustin@gmail.com>
2019-03-16 00:16:35 -0500

    Add files via upload

---
+++ Celiac Now/Pods/Target Support Files/Pods-Celiac Now/Pods-Celiac Now-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Celiac_Now : NSObject
+@end
+@implementation PodsDummy_Pods_Celiac_Now
+@end
---
+++ Celiac Now/Pods/Target Support Files/Pods-Celiac NowTests/Pods-Celiac NowTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Celiac_NowTests : NSObject
+@end
+@implementation PodsDummy_Pods_Celiac_NowTests
+@end
